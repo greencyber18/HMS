392b38ce9a57874337e3817bb9a2e78b17310b65
greencyber18 <rafshan.nds@gmail.com>
2021-12-09 15:00:30 +0600

    Commit all

---
+++ HMS/BEL/TokenModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEL
+{
+    public class TokenModel
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public string AccessToken { get; set; }
+        public Nullable<System.DateTime> CreatedAt { get; set; }
+        public Nullable<System.DateTime> ExpiredAt { get; set; }
+    }
+}
---
+++ HMS/DAL/DataAccessFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DataAccessFactory
+    {
+        static HomeRentEntities db;
+        static DataAccessFactory()
+        {
+            db = new HomeRentEntities();
+        }
+       
+        public static IRepository<User, int> UserDataAccess()
+        {
+            return new UserRepo(db);
+        }
+        public static IRepository<Token, string> TokenDataAccess()
+        {
+            return new TokenRepo(db);
+        }
+        public static IAuth AuthDataAccess() {
+            return new UserRepo(db);
+        }
+    }
+}
